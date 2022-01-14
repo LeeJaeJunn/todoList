d2c5d6b77ddcfa6e3e2d9dfcf3b9404c603f7229
LeeJaeJunn <leejjun00@gmail.com>
2022-01-15 07:10:41 +0900

    🔥 [todo-list] 필요없는 파일, 데이터 삭제

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import { Box } from 'native-base';
+import { Box, VStack, Text, HStack } from 'native-base';
 import Todo from './component/todo';
 import DayList from './component/day-list';
-import Header from './component/header';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import CreateTodo from './component/create-todo';
 import CreateDay from './component/create-day';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <Box px={'150px'} py={'40px'} minH={'100vh'} minW={'100vw'} bgColor={'#8ed1fc'}>
       <BrowserRouter>
         <Box bgColor={'#ffffff'} minH={'50vh'} minW={'60vw'} borderRadius={'30px'}>
-          <Header />
+          <VStack px={'30px'} py={'30px'} justifyContent={'space-between'} space={10}>
+            <Box alignItems={'center'}>
+              <Link to={'/'}>
+                <Text fontSize={'50px'} fontWeight={'bold'} color={'black'}>
+                  나의 Todo-List
+                </Text>
+              </Link>
+            </Box>
+            <Box alignItems={'center'}>
+              <HStack space={'10px'} alignItems={'center'}>
+                <Link to={'/create_day'}>
+                  <Box
+                    px={'30px'}
+                    py={'15px'}
+                    bgColor={'#1a237e'}
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    borderRadius={'20px'}
+                  >
+                    <Text fontSize={'20px'}>날짜 추가</Text>
+                  </Box>
+                </Link>
+                <Link to={'/create_todo'}>
+                  <Box
+                    px={'30px'}
+                    py={'15px'}
+                    bgColor={'#1a237e'}
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    borderRadius={'20px'}
+                  >
+                    <Text fontSize={'20px'}>할일 추가</Text>
+                  </Box>
+                </Link>
+              </HStack>
+            </Box>
+          </VStack>
           <Switch>
-            <Route exact path='/'>
+            <Route exact path={'/'}>
               <DayList />
             </Route>
-            <Route path='/day/:day'>
+            <Route path={'/day/:day'}>
               <Todo />
             </Route>
             <Route path={'/create_todo'}>
@@ -33,13 +69,3 @@ function App() {
 }
 
 export default App;
-
-// json-server --watch ./src/_dummy/data.json --port 3001
-
-/*
-  REST API
-  Create : POST
-  Read : GET
-  Update : PUT
-  Delete : DELETE
-*/
